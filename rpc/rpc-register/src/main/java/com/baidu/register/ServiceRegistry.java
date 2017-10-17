13667e469c0e69416b33ae1c5a3e44f449cc53c0
liweiyuan <535767595@qq.com>
2017-10-17 14:19:34 +0800

    add register

---
+++ rpc/rpc-register/src/main/java/com/baidu/register/ServiceRegistry.java
@@ -0,0 +1,7 @@
+package com.baidu.register;
+
+/**
+ * Created by tingyun on 2017/10/17.
+ */
+public interface ServiceRegistry {
+}
---
+++ rpc/rpc-zookeeper-register/src/main/java/com/baidu/zookeeper/register/ZooKeeperServiceRegistry.java
@@ -0,0 +1,13 @@
+package com.baidu.zookeeper.register;
+
+/**
+ * Created by tingyun on 2017/10/17.
+ */
+
+import com.baidu.register.ServiceRegistry;
+
+/**
+ * ServiceRegistry:注册中心的接口服务
+ */
+public class ZooKeeperServiceRegistry implements ServiceRegistry {
+}
