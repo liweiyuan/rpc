6f694fd4aeafc01c033d1e2b6f307eafb4af211b
liweiyuan <535767595@qq.com>
2017-10-17 18:58:56 +0800

    add 反序列化

--- rpc/rpc-common/src/main/java/com/baidu/common/util/SerializationUtil.java
+++ rpc/rpc-common/src/main/java/com/baidu/common/util/SerializationUtil.java
@@ -1,13 +1,59 @@
 package com.baidu.common.util;
 
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
 /**
  * Created by tingyun on 2017/10/17.
  * 序列化工具类（基于 Protostuff 实现）
  */
 public class SerializationUtil {
 
+    private static Map<Class<?>, Schema<?>> cachedSchema = new ConcurrentHashMap<>();
 
-    public static Object deserialize(byte[] data, Class<?> genericClass) {
-        return data;
+    private static Objenesis objenesis = new ObjenesisStd(true);
+
+    public SerializationUtil() {
+    }
+
+    /**
+     * 反序列化（字节数组 -> 对象）
+     *
+     * @param data
+     * @param genericClass
+     * @return
+     */
+    public static <T> T deserialize(byte[] data, Class<T> genericClass) {
+        //return data;
+        try {
+            T message = objenesis.newInstance(genericClass);
+            Schema<T> schema = getSchema(genericClass);
+            ProtostuffIOUtil.mergeFrom(data, message, schema);
+            return message;
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+
+    }
+
+
+    /**
+     * 序列化（对象 -> 字节数组）
+     */
+
+    private static <T> Schema<T> getSchema(Class<T> genericClass) {
+        Schema<T> schema = (Schema<T>) cachedSchema.get(genericClass);
+        if (schema == null) {
+            schema = RuntimeSchema.createFrom(genericClass);
+            cachedSchema.put(genericClass, schema);
+        }
+        return schema;
     }
 }
