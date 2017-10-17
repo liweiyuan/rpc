4822d3a8b509c729340fd7deb08977f0266ee472
liweiyuan <535767595@qq.com>
2017-10-17 11:33:57 +0800

    add rpc-sample-api and rpc-server

--- rpc/rpc-sample-api/src/main/java/com/baidu/sample/api/HelloService.java
+++ rpc/rpc-sample-api/src/main/java/com/baidu/sample/api/HelloService.java
@@ -4,4 +4,7 @@ package com.baidu.sample.api;
  * Created by tingyun on 2017/10/17.
  */
 public interface HelloService {
+    String hello(String name);
+
+    String hello(Person person);
 }
---
+++ rpc/rpc-sample-api/src/main/java/com/baidu/sample/api/Person.java
@@ -0,0 +1,33 @@
+package com.baidu.sample.api;
+
+/**
+ * Created by tingyun on 2017/10/17.
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
